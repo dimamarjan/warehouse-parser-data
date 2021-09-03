ba06ae56cc4a3eba2e75442343c3a3da7ab5e6fc
Dmitriy Marianchenko <dima.marjan@gmail.com>
2021-09-03 07:27:02 +0300

    fix

--- parser.py
+++ parser.py
@@ -1,6 +1,5 @@
 import json
 from docx import Document
-from whServer import start_server
 from locationVoskresen import locationAddres
 
 
@@ -16,62 +15,67 @@ def build_file(loaded_file):
 
 
 def split_positions(data_list):
-    splied_list = []
+    split_list = []
     temp = []
     for k in data_list:
         temp.append(k)
         if len(temp) == 4:
-            splied_list.append(temp)
+            split_list.append(temp)
             temp = []
-    return splied_list
+    return split_list
 
 
-def compare_lists(list1, list2=""):
+def compare_lists(list1, list2=None):
+    if list2 is None:
+        list2 = []
     dataList = []
 
     try:
-        # position_dict = {}
-        # location = []
+        location = []
         for position in list1:
             location = position[1].split()
             dataList.append({
-                            "num": position[0],
-                            "cargo": "false",
-                            "changeQ": "false",
-                            "changedQTo": int(0),
-                            "name": position[1],
-                            "quantity": position[2],
-                            "location": locationAddres(location[len(location) - 1]),
-                            "uhtishka": "false",
-                            "uhtQuant": int(0)
+                "id": position[0],
+                "cargo": bool(0),
+                "changeQ": bool(0),
+                "changedQTo": int(0),
+                "name": position[1],
+                "quantity": position[2],
+                "location": locationAddres(location[len(location) - 1]),
+                "uhtishka": bool(0),
+                "uhtQuant": int(0)
             })
 
         if len(list2) != 0:
             for position in list2:
                 location = position[1].split()
                 dataList.append({
-                    "num": str((int(position[0]) + len(list1))),
-                    "cargo": "false",
-                    "changeQ": "false",
+                    "id": str((int(position[0]) + len(list1))),
+                    "cargo": bool(0),
+                    "changeQ": bool(0),
                     "changedQTo": int(0),
                     "name": position[1],
                     "quantity": position[2],
                     "location": locationAddres(location[len(location) - 1]),
-                    "uhtishka": "false",
+                    "uhtishka": bool(0),
                     "uhtQuant": int(0)
                 })
-        return dataList
+        buildedData = {"positions": dataList}
+        return buildedData
     except IndexError as err:
         dataList = 0
         print("comparing error: ", err)
-        return dataList
+        buildedData = {"positions": dataList}
+        return buildedData
 
 
 if __name__ == "__main__":
+    split_list1 = []
+    split_list2 = []
     try:
-        document1 = Document("./docx/tempFile-t.docx")
+        document1 = Document('./docx/tempFile-t.docx')
         input_data1 = build_file(document1)
-        splited_list1 = split_positions(input_data1)
+        split_list1 = split_positions(input_data1)
     except BaseException as error:
         print("file-1: ", error)
         input("Файл tempFile-t.docx не найден...")
@@ -79,15 +83,17 @@ if __name__ == "__main__":
     try:
         document2 = Document("./docx/tempFile-v.docx")
         input_data2 = build_file(document2)
-        splited_list2 = split_positions(input_data2)
+        split_list2 = split_positions(input_data2)
     except BaseException as error:
         print("file-2: ", error)
-    positions_compared = compare_lists(splited_list1, splited_list2)
+    if len(split_list2) != 0:
+        positions_compared = compare_lists(split_list1, split_list2)
+    else:
+        positions_compared = compare_lists(split_list1)
 
     if positions_compared != 0:
-        with open("data.json", "w+", encoding="utf-8") as f:
+        with open("./wh-data-serv/db.json", "w+", encoding="utf-8") as f:
             json.dump(positions_compared, f, ensure_ascii=False)
     else:
         pass
 
-    start_server()
